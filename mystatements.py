ad288cc43fd695314f12bec841db3fcb67d4b690
Rhod-lab <55310735+Rhod-lab@users.noreply.github.com>
2025-05-04 20:19:39 +0000

    adding file to repo

---
+++ mystatements.py
@@ -0,0 +1,11 @@
+my_list = ["Apple", "Banana", "Cherry"]
+my_dict = {"orange":"coffee", "milk": "whole"}
+
+for item in my_list:
+    print(f"My favorite fruit is {item}")
+
+for key, value in my_dict.items():
+    print(f"My favorite think to drink is {key}")
+
+# my_dict = my_dict
+# my_list2 = my_list
