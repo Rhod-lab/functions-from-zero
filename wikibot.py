c7dfead22d6450dc0a685fc60407afce435d93c1
Rhod-lab <55310735+Rhod-lab@users.noreply.github.com>
2025-05-09 18:08:58 +0000

    adding function

--- wikibot.py
+++ wikibot.py
@@ -1,14 +1,17 @@
 import wikipedia
 
-import click
+# import click
 
-@click.command()
-@click.option('--name', prompt='wikipedia page to scrape',
-                help='web page we want to scrape')
+# @click.command()
+# @click.option('--name', prompt='wikipedia page to scrape',
+#                help='web page we want to scrape')
 
 def scrape (name="Microsoft", length=1):
     result = wikipedia.summary(name, sentences=length)
-    click.echo(click.style(f"{result}:", fg="blue"))
+    return result
+print(scrape("wikipedia"))
 
-if __name__== '__main__':
-    scrape()
+#   click.echo(click.style(f"{result}:", fg="blue"))
+
+#if __name__== '__main__':
+#    scrape()
