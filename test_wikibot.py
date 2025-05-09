8cf81632c0805120820b308d020cef0a0c2e7e60
Rhod-lab <55310735+Rhod-lab@users.noreply.github.com>
2025-05-09 19:41:23 +0000

    adding tests for cli

--- test_wikibot.py
+++ test_wikibot.py
@@ -7,6 +7,6 @@ def test_scrape():
 
 def test_wikibot():
     runner = CliRunner()
-    result = runner.invoke(cli, ['--name', 'Microsoft', '--length', '1'])
+    result = runner.invoke(cli, ['--name', 'Microsoft'])
     assert result.exit_code == 0
-    assert "Microsoft" in scrape("Microsoft")
+    assert "Microsoft" in result.output
--- wikibot.py
+++ wikibot.py
@@ -4,12 +4,9 @@ from mylib.bot import scrape
 @click.command()
 @click.option('--name',
               help='web page we want to scrape')
-@click.option('--length',
-              help='length of the output from wipedia')
-
-def cli(name, length):
-    result = scrape(name, length=length)
-    click.echo(click.style(f"{result}:", bg="green", fg="white"))
+def cli(name):
+    result = scrape(name)
+    click.echo(click.style(f"{result}:", fg="white"))
 
 if __name__== '__main__':
     cli()
